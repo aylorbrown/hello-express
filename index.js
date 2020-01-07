037433675804a27f79fde5bdf6eeeb5fe6dc6a7a
Aylor Brown <aylorbrown@gmail.com>
2020-01-07 17:17:14 -0500

    add route to homepage

--- index.js
+++ index.js
@@ -2,8 +2,19 @@
 const http = require('http');
 const PORT = 3000; // variable for the port 
 
+const express = require('express');
+const app = express();
+// app is result of calling express 
+
 // call createServer 
-const server = http.createServer();
+const server = http.createServer(app);
+
+//homepage?
+app.get('/', (req, res) => {
+    console.log(`Got a request!`);
+    res.send(`Hello Express!`);
+});
+
 
 
 // have server listen 
